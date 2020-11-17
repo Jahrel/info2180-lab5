021e5a08661bb3508034eb00be1d0c5414220537
Jahrel Mckenzie <jahr3lm@gmail.com>
2020-11-17 18:21:33 -0500

    Added serach for list of conuntries through update query

--- world.php
+++ world.php
@@ -1,13 +1,18 @@
-<?php
+<?php header("Access-Control-Allow-Origin: *");
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
-$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  $country = filter_input(INPUT_GET, "country", FILTER_SANITIZE_STRING);
+  $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+  $country = $_GET["country"];
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
 
 ?>
 <ul>
@@ -15,3 +20,5 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
 <?php endforeach; ?>
 </ul>
+
+                  
